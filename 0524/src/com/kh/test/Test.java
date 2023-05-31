d5c54ea9bde6cd71f1f0a44ddad568e8af4ae575
YongJinPark91 <skylock45@naver.com>
2023-05-31 12:11:33 +0900

    12
    12

--- 0524/src/com/kh/test/Test.java
+++ 0524/src/com/kh/test/Test.java
@@ -5,6 +5,10 @@ public class Test {
 	public void method1() {
 		System.out.println("12");
 		System.out.println("11");
+		System.out.println("hi");
+		System.out.println("¾È³ç");
+		System.out.println("ÇÏ¼¼");
+		System.out.println("¿ä?");
 		
 	}
 
